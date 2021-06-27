9f98a053b2a0d8417e76c14bee6a47ae5a1c4b44
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-27 13:32:16 +0530

    Shadow Music

--- DaisyXMusic/helpers/admins.py
+++ DaisyXMusic/helpers/admins.py
@@ -20,8 +20,8 @@ from typing import List
 from pyrogram.types import Chat
 from pyrogram.types import User
 
-from DaisyXMusic.function.admins import get as gett
-from DaisyXMusic.function.admins import set
+from ShadowMusic.function.admins import get as gett
+from ShadowMusic.function.admins import set
 
 
 async def get_administrators(chat: Chat) -> List[User]:
