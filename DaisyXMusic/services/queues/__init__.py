11623bf27a9dbc226b9a43e30d64241d2d4a58e6
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-28 14:03:56 +0530

    Shadow Music

--- DaisyXMusic/services/queues/__init__.py
+++ DaisyXMusic/services/queues/__init__.py
@@ -1,7 +1,7 @@
-from DaisyXMusic.services.queues.queues import clear 
-from DaisyXMusic.services.queues.queues import get
-from DaisyXMusic.services.queues.queues import is_empty
-from DaisyXMusic.services.queues.queues import put
-from DaisyXMusic.services.queues.queues import task_done
+from ShadowMusic.services.queues.queues import clear 
+from ShadowMusic.services.queues.queues import get
+from ShadowMusic.services.queues.queues import is_empty
+from ShadowMusic.services.queues.queues import put
+from ShadowMusic.services.queues.queues import task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
