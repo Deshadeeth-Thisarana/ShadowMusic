26ca1a36b90336ac9e671e0184f7c12a8f13df90
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-28 14:00:58 +0530

    Shadow Music

--- DaisyXMusic/services/callsmusic/__init__.py
+++ DaisyXMusic/services/callsmusic/__init__.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 
-from DaisyXMusic import config
+from ShadowMusic import config
 
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 run = client.run
