fd6603fc71483f664c2ec5b4db9654173c0438cc
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-07-13 08:36:54 +0530

    Shadow Music

--- ShadowMusic/function/admins.py
+++ ShadowMusic/function/admins.py
@@ -6,8 +6,7 @@ admins: Dict[int, List[int]] = {}
 def set(chat_id: int, admins_: List[int]):
     admins[chat_id] = admins_
 
-def get(chat_id: int) -> Union[List[int], bool]:
+def get(chat_id: int) -> List[int]:
     if chat_id in admins:
         return admins[chat_id]
-
-    return False
+    return []
