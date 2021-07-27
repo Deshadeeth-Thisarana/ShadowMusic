24f53e3db9c2704aa8ef79afdd85bac9732704a3
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-07-27 14:08:12 +0530

    Shadow Music

--- ShadowMusic/modules/play.py
+++ ShadowMusic/modules/play.py
@@ -1,6 +1,7 @@
 # Shadow Music (Telegram bot project)
 # Copyright (C) 2021 Deshadeeth Thisarana
 # Copyright (C) 2021  TheHamkerCat (Python_ARQ)
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
