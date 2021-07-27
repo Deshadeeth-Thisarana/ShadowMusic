8a9fb41a15d03e0b9426bdf2d9000936a7f429da
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-07-27 13:30:19 +0530

    Update queues.py

--- ShadowMusic/services/queues/queues.py
+++ ShadowMusic/services/queues/queues.py
@@ -1,3 +1,6 @@
+# 2021 ShadowMusic
+# 2020 - 2021 DeshadeethThisarana
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
