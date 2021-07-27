87573aa1835f6d8062c070883ed950c108cf0aeb
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-07-27 13:34:34 +0530

    Shadow Music

--- ShadowMusic/services/queues/__init__.py
+++ ShadowMusic/services/queues/__init__.py
@@ -1,7 +1,6 @@
-from ShadowMusic.services.queues.queues import clear 
-from ShadowMusic.services.queues.queues import get
-from ShadowMusic.services.queues.queues import is_empty
-from ShadowMusic.services.queues.queues import put
-from ShadowMusic.services.queues.queues import task_done
+# (c) copyright 2021 ShadowMusic
+# (c) copyright 2020 - 2021 DeshadeethThisarana
+
+from ShadowMusic.services.queues.queues import clear, get, is_empty, put, task_done 
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
