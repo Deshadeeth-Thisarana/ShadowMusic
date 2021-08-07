692c655a48e340580b7a038f587caf469abd4482
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-08-07 17:11:43 +0530

    Update song.py

--- ShadowMusic/modules/song.py
+++ ShadowMusic/modules/song.py
@@ -1,5 +1,5 @@
 # Shadow Music (Telegram bot project )
-# Copyright (C) 2021 Deshadeeth Thisarana
+# Copyright (C) 2020 - 2021 Deshadeeth Thisarana
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -76,7 +76,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by Shadow Music**"
+        rep = "**🎵 Uploaded by @Mr_Shadow_Robot**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
