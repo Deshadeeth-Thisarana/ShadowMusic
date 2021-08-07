9841ac5641c5020b90398befe7700a3baceaf9d6
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-08-07 13:42:40 +0530

    Update __main__.py

--- ShadowMusic/__main__.py
+++ ShadowMusic/__main__.py
@@ -1,5 +1,5 @@
 # ShadowMusic (Telegram bot project)
-# Copyright (C) 2021 DeshadeethThisarana & Rojserbest
+# Copyright (C) 2021 DeshadeethThisarana & Damantha Jasinghe
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -19,7 +19,7 @@ import requests
 from pyrogram import Client as Bot
 
 from ShadowMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from ShadowMusic.services.callsmusic.callsmusic import run
+from ShadowMusic.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
